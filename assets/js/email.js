63c0cd2853d6fbe0a295e2e65dbe371017c932a5
Vicente González <vicente19600@gmail.com>
2025-08-11 18:59:17 +0200

    refactor: update using cloud sonet model

--- assets/js/email.js
+++
@@ -1,7 +0,0 @@
-$("#contactForm").submit(function(event){
-    var form = $(this);
-    form.fadeOut('fast', function() {
-        form.parent('#container-form').attr("style","margin-top: 80px;");
-        $( "#msgSubmit" ).removeClass( "hidden" );
-    });
-});
